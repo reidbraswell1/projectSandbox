558a70ac38069a8d1d22668098dff7d083afa285
reid <reidbras@gmail.com>
2018-05-23 19:48:29 -0500

    Updated .gitignore and Program.cs. Sandbox will now store api token in a file that will be prompted for. Search terminated with tilda.

--- Program.cs
+++ Program.cs
@@ -15,11 +15,9 @@ namespace projectSandbox
 {
     class Program
     {
-        private static string token = "";
+        private static string token;
         private static string api = "https://api.ebay.com/buy/browse/v1/item_summary/search";
-        private static string options = "?q=pear&limit=3";
-
-
+        private static readonly string _apiKeyFileName = $"{Environment.CurrentDirectory}/apiKey";
         private static string HttpGet(string url)
         {
             try
@@ -38,12 +36,22 @@ namespace projectSandbox
 
                 var json = reader.ReadToEnd();
                 var items = JsonConvert.DeserializeObject<RootObject>(json);
-                var itemsList = items.itemSummaries;
-                foreach(var item in itemsList)
+                if (items.total > 0)
                 {
-                    Console.WriteLine(item.title);
-                    Console.WriteLine(item.price.value);
-                    Console.WriteLine(item.image.imageUrl);
+                    var itemsList = items.itemSummaries;
+                    var cnt = 1;
+                    foreach (var item in itemsList)
+                    {
+                        Console.WriteLine($"\n({cnt++}).    Item Title = {item.title}");
+                        Console.WriteLine($"\t Item ID        = {item.itemId}");
+                        Console.WriteLine($"\t Item Web Url   = {item.itemWebUrl}");
+                        Console.WriteLine($"\t Item Price     = {item.price.value}");
+                        Console.WriteLine($"\t Item Image URl = {item.image.imageUrl}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Items Found");
                 }
 
                 reader.Close();
@@ -58,31 +66,50 @@ namespace projectSandbox
                 return "Error: " + ex.Message;
             }
         }
-        /* 
-        private static string HttpGet(string URI)
+        private static string GetAPIKey(String path)
         {
-            WebClient client = new WebClient();
-
-            // Add a user agent header in case the 
-            // requested URI contains a query.
-
-            client.Headers.Add("Authorization", "Bearer v^1.1#i^1#r^0#I^3#p^1#f^0#t^H4sIAAAAAAAAAOVXf2wTVRxf98vMORX5ISwYy4F/gN713V3v2p60oawjazK2QTvAocHr3Tt2rL2r917XVY0ZIw4k0YiDAIJkigkhwSgxRBETTIgKMQEWIVFMCAGiREM0BgViJr67ltFNws8hJPaf5r7v+77v8/l8f9w90F1ZNaO3ofd8jeu+0v5u0F3qcrHVoKqy4skHy0prK0pAkYOrv3tad3lP2ZmZSE4l09J8iNKmgaC7K5U0kOQYg1TGMiRTRjqSDDkFkYQVKRae2yhxDJDSlolNxUxS7mgkSPG+hBewIptQuYCmsCKxGpdjxs0g5ec1AIAsQ6/IspqmknWEMjBqICwbOEhxgPXTQKA5Ls6KEvBKXh8T4H1tlHsBtJBuGsSFAVTIgSs5e60irNeGKiMELUyCUKFoeE6sORyN1DfFZ3qKYoUKOsSwjDNo+FOdqUL3AjmZgdc+BjneUiyjKBAhyhPKnzA8qBS+DOYW4DtSJ7yi6NO8ssipgQQvJEZFyjmmlZLxtXHYFl2lNcdVggbWce56ihI1EsugggtPTSRENOK2/+Zl5KSu6dAKUvWzw8+EW1qoUGNzazQWaY3TC3XU3qgjTLfMj9C8KPoFoGoaLaoCL8uaUjgoH60g84iT6kxD1W3RkLvJxLMhQQ2HayNIQpE2xKnZaLbCGrYRFfv5hzRk2+yk5rOYwe2GnVeYIkK4ncfrZ2BoN8aWnshgOBRh5IIjUZCS02ldpUYuOrVYKJ8uFKTaMU5LHk82m2WyPGNaSz0cAKxn0dzGmNIOUzJFfO1ez/vr199A6w4VBZKdSJdwLk2wdJFaJQCMpVSI8/Mixxd0Hw4rNNL6L0MRZ8/wjhitDhH9ZMaoPiUg8CDAa/xodEioUKQeGwdMyDk6JVsdEKeTsgJphdRZJgUtXZV4QeN4vwZpVQxotDdAyjYhqCLNahACCBMJJeD/PzXKjZZ6TDHTsMVM6kpuVAp+1Iqdt9QW2cK5GEwmieFGq/6qJJFN8o7Ts3v9pijaMRAJIqd1xq5tRjFTHlMmQ802LXFQ3xZvnbwP76mkEoJ5prqaf5ExDl0GdSqMBZGZscg7nGm253rc7IAG6RJsmckktBawt6XE6E30uzTNr8pKSepExiX3GrObHJO3WNsyvousy3tci6/CnBU4HwAC8Ptui1udk9d47j8YWjeV2AYTYajegQ8Qz/DrUKjE+bE9rl2gx7WT3KiABzzBTgVTKstay8seqEU6howuawzSlxrkK9+CTAfMpWXdKq10LZ780fYlRRew/ufAxKErWFUZW110HwOTr6xUsA89WsP6gcBxrAi8Xl8bmHpltZydUD7u2PH3BqvSJ/S3Htlwckes//DEU32LQM2Qk8tVUUIqo2Tlqvjpjs9XDJy9//HOv7KsMZD948zP/N623dMbGq0fkEvcOW/xyr59uT8P1Eu51w+9MVBR3cu8c+nUsVkfjNn0/sYJs6oqxm84nBoYs4P//rGx67b1nn2qUogwmzaP6Tpo7Qm+NsW75aWLLxrLfvyQ+7ukb/Xe35J157pqP97vm/bQ2m/HDa5+tXEqv+V8cNK+2udPfuXvrNmy7pU9G6Sv6948N7imde3Wo0fW/PIdv2PX5umnf8cHWvfNylSPnfPrs52fHflm/7aajSe3bt89eEFdicf3ZQ6uSmWPPj0pekiVLmZ/Ot717oxzy5u/uNRQv9y3e4XadAFF1h+Iv/zCifELv2SYyNsPr//0k417LuXT9w+Ve+uAGg8AAA==");
-            client.Headers.Add("Content-Type", "application/json");
-            client.Headers.Add("X-EBAY-C-ENDUSERCTX", "affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>");
-
-            Stream data = client.OpenRead(URI);
-            StreamReader reader = new StreamReader(data);
-            string s = reader.ReadToEnd();
-            data.Close();
-            reader.Close();
-
-            return s;
+            var resp = "";
+            do
+            {
+                Console.WriteLine("Do You Wish To Update The API Token");
+                resp = Console.ReadLine();
+            } while (!resp.ToLower().Equals("y") && !resp.ToLower().Equals("n"));
+            if (resp.ToLower().Equals("y"))
+            {
+                do
+                {
+                    Console.WriteLine("Enter Text");
+                    resp = Console.ReadLine();
+                } while (String.IsNullOrEmpty(resp));
+                File.WriteAllText(path, resp);
+            }
+            return (File.Exists(path)) ? File.ReadAllText(path) : null;
         }
-        */
+
         static void Main(string[] args)
         {
+            token = GetAPIKey(_apiKeyFileName);
+            if (String.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("API KEY IS NULL EXITING");
+                Environment.Exit(1);
+            }
             //HttpGet(api + options);
-            HttpGet(api + options);
+            while (true)
+            {
+                var response = "";
+                do
+                {
+                    Console.WriteLine("Enter Description ~ Exits");
+                    response = Console.ReadLine();
+                } while (String.IsNullOrEmpty(response));
+                if (response.Equals("~"))
+                {
+                    break;
+                }
+                var options = $"?q={response}&limit=15";
+                HttpGet(api + options);
+            }
         }
     }
 }
